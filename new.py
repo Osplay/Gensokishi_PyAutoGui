c7b74ea7086258ab9914b83d46fb7a92c5545d7a
Osplay <efollin@protonmail.com>
2022-12-25 22:01:51 -0300

    test solo player

--- new.py
+++ new.py
@@ -8,6 +8,7 @@ PATH_IMG = PATH + r"\img"
 
 VAR_ATTACK_BUTTON_POSITION = None
 VAR_PARTY_BUTTON_POSITION = None
+VAR_SCREEN_CENTER = None
 
 PATH_BUTTON_ATTACK_IDLE = PATH_IMG+r"\attack_idle.png"
 PATH_BUTTON_PARTY = PATH_IMG + r"\party.png"
@@ -20,6 +21,11 @@ class PLAYER_ACTION(Enum):
     PARTY_COMBAT = 2
     UNKNOW = 3
 
+class PLAYER_DIRECTIONS(Enum):
+    UP = 0,
+    RIGHT = 1,
+    DOWN = 2,
+    LEFT = 3
 
 def SetButtonPosition(name_button, path):
     print(f"Configurando posición del botón {name_button}...")
@@ -58,7 +64,48 @@ def PerformAttack():
         pyautogui.moveTo(VAR_ATTACK_BUTTON_POSITION)
         pyautogui.click(VAR_ATTACK_BUTTON_POSITION)
 
+def ButtonExist(button, confidence):
+    if pyautogui.locateAllOnScreen(image=button, confidence=confidence) is None:
+        return False
+    else:
+        return True
+
+def PlayerMoveSteps(direction, steps):
+    #Uso la posición del centro
+    global VAR_SCREEN_CENTER
+
+    #muevo el puntero al medio
+    pyautogui.moveTo(VAR_SCREEN_CENTER)
+
+    #espero x tiempo
+    time.sleep(0.2)
+
+    var_player_x = VAR_SCREEN_CENTER[0]
+    var_player_y = VAR_SCREEN_CENTER[1]
+
+    #muevo x pixel hacia el lado indicado
+    if direction == PLAYER_DIRECTIONS.UP:
+        var_player_y -= steps
+    elif(direction == PLAYER_DIRECTIONS.RIGHT):
+        var_player_x += steps
+    elif(direction == PLAYER_DIRECTIONS.DOWN):
+        var_player_y += steps
+    else:
+        var_player_x -= steps
+
+    pyautogui.moveTo(var_player_x, var_player_y)
+    time.sleep(0.2)
+    pyautogui.click()
+    time.sleep(0.5)
+        
+# Start Script
 def main():
+    global VAR_SCREEN_CENTER
+
+    temp_screen = pyautogui.size()
+    VAR_SCREEN_CENTER = (round(temp_screen.width / 2), round(temp_screen.height / 2))
+
+
     print("1) Party")
     print("2) Solo")
     value = input()
@@ -68,6 +115,8 @@ def main():
     else:
         ScriptSolo()
 
+
+## Scripts --------
 def ScriptSolo():
     global VAR_ATTACK_BUTTON_POSITION
     global VAR_PARTY_BUTTON_POSITION
@@ -77,11 +126,36 @@ def ScriptSolo():
 
     VAR_ATTACK_BUTTON_POSITION = SetButtonPosition("ataque", PATH_BUTTON_ATTACK_IDLE)
 
+    var_steps_min = 5 # minimum of step to move the mouse.
+    var_place_to_torn = PLAYER_DIRECTIONS.UP # Posible directions of the player.
+
     while True:
         
         #Script Temporal para sólo atacar según el botón de party
 
-        PerformAttack()
+        #Jugador está en combate.
+        action = CheckPlayerAction()
+
+        if action == PLAYER_ACTION.COMBAT:
+            #Hacer ataque.
+            PerformAttack()
+        else:
+            while ButtonExist(PATH_BUTTON_ATTACK_IDLE, 0.65) is True:
+                #Muevo al jugador a la posición y reseteo
+                PlayerMoveSteps(var_place_to_torn, var_steps_min)
+
+                if var_place_to_torn == PLAYER_DIRECTIONS.UP:
+                    var_place_to_torn = PLAYER_DIRECTIONS.RIGHT
+                elif(var_place_to_torn == PLAYER_DIRECTIONS.RIGHT):
+                    var_place_to_torn = PLAYER_DIRECTIONS.DOWN
+                elif(var_place_to_torn == PLAYER_DIRECTIONS.DOWN):
+                    var_place_to_torn = PLAYER_DIRECTIONS.LEFT
+                else:
+                    var_place_to_torn = PLAYER_DIRECTIONS.UP
+                    var_steps_min += 2 #agregamos para movernos más espacios.
+                
+                PerformAttack() #Verificamos si se puede atacar
+                time.sleep(0.5)
 
         time.sleep(3) # tiempo prudencial cuando estoy haciendo debugging
 
@@ -91,6 +165,7 @@ def ScriptParty():
     global VAR_PARTY_BUTTON_POSITION
     global PATH_BUTTON_ATTACK_IDLE
     global PATH_BUTTON_PARTY
+    global VAR_SCREEN_CENTER
     
 
     VAR_ATTACK_BUTTON_POSITION = SetButtonPosition("ataque", PATH_BUTTON_ATTACK_IDLE)
@@ -114,8 +189,15 @@ def ScriptParty():
             pyautogui.click(VAR_PARTY_BUTTON_POSITION)
             time.sleep(0.2)
             pyautogui.moveTo(VAR_ATTACK_BUTTON_POSITION)
-            time.sleep(0.2)
             pyautogui.click(VAR_ATTACK_BUTTON_POSITION)
+            time.sleep(0.2)
+            action = CheckPlayerAction()
+
+            if action is not PLAYER_ACTION.COMBAT:
+                pyautogui.moveTo(VAR_SCREEN_CENTER)
+                pyautogui.click(VAR_SCREEN_CENTER)
+                print("No está en el centro!")
+
 
 
         time.sleep(3) # tiempo prudencial cuando estoy haciendo debugging
